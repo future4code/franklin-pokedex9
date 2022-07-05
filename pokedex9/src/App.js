8bef343c9624112552e89f87da6bfd4ba6633bf7
Victor Ramon <victoramonfm@gmail.com>
2022-07-05 15:58:36 -0300

    Adicionando requisições

--- pokedex9/src/App.js
+++ pokedex9/src/App.js
@@ -1,9 +1,14 @@
-
+import { getPokemonDetails, getPokemons } from "./services/requests";
 
 function App() {
+
+  const id = 1
+
   return (
     <div >
       <h1>Pokedex</h1>
+      <button onClick={getPokemons}>getPokemons</button>
+      <button onClick={() => {getPokemonDetails(id)}}>getPokemonDetails</button>
     </div>
   );
 }
